773dfbce34b8986355b3184f59ab610d93a9f654
Ngochinh-bapngo <ngo85870@gmail.com>
2021-01-11 22:18:00 +0700

    Them phan giao dien index va sua lai trang create,login

---
+++ client/main.js
@@ -0,0 +1,22 @@
+const listPersonal = document.querySelector(".list-user");
+//console.log(listPersonal);
+fetch("http://localhost/Cv_online/api/personal/getAllPersonal.php", {
+        method: "GET",
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        data.forEach((element) => {
+            let user = `<div class="user">
+            <div class="img">
+                <img src="${element.avatar}" alt="avatar">
+            </div>
+            <div class="text">
+                <h3>${element.fullname}</h3>
+                <a href="http://localhost/CV/client/view.html?personal_id=${element.personal_id}">Xem ngay <i class="fas fa-angle-double-right"></i></a>
+            </div>
+        </div>`;
+
+            listPersonal.innerHTML += user;
+        });
+    });
