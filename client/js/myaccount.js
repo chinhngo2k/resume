028ef1e8b134e7f6bee7c34a3b83ccc6f5dc11cd
Ngochinh-bapngo <ngo85870@gmail.com>
2021-01-16 10:41:33 +0700

    Sua giao dien trang myaccount

--- client/js/myaccount.js
+++ client/js/myaccount.js
@@ -1,18 +1,19 @@
-const id = sessionStorage.getItem('id');
+const id = sessionStorage.getItem("id");
 console.log(id);
-if(!id){
+if (!id) {
     window.location.href = "http://localhost/resume/client/login.html";
-}
-else{
-    console.log('dang nhap thanh cong');
+} else {
+    console.log("dang nhap thanh cong");
 }
 
 const listPersonal = document.querySelector(".list-cv");
 
 let data = [];
 
-(async () => {
-    const res = await fetch(`http://localhost/resume/server/api/personal/getPersonalByAccount.php?acc_id=${id}`);
+(async() => {
+    const res = await fetch(
+        `http://localhost/resume/server/api/personal/getPersonalByAccount.php?acc_id=${id}`
+    );
     data = await res.json();
 
     console.log(data);
@@ -26,23 +27,24 @@ let data = [];
             <h3>${element.fullname}</h3>
             <a href="http://localhost/resume/client/view.html?personal_id=${element.personal_id}">Xem ngay <i class="fas fa-angle-double-right"></i></a>
         </div>
-        <div>
-            <a href="http://localhost/resume/client/editresume.html?personal_id=${element.personal_id}">chinh sua</a>
-            <button type="button" class="btn-delete" id="${element.personal_id}">xoa</button>
+        <div class="button">
+           <button> <a href="http://localhost/resume/client/editresume.html?personal_id=${element.personal_id}">Sửa</a></button>   
+            <button type="button" class="btn-delete" id="${element.personal_id}">Xóa</button>
         </div>
     </div>`;
 
         listPersonal.innerHTML += user;
     });
-
 })();
 
-listPersonal.onclick = async (e) => {
+listPersonal.onclick = async(e) => {
     item = e.target;
     console.log(item);
-    if(item.getAttribute('type') == 'button'){
-        let id = item.getAttribute('id');
-        await fetch(`http://localhost/resume/server/api/resume/deleteResume.php?per_id=${id}`);
+    if (item.getAttribute("type") == "button") {
+        let id = item.getAttribute("id");
+        await fetch(
+            `http://localhost/resume/server/api/resume/deleteResume.php?per_id=${id}`
+        );
         location.reload();
     }
-}
+};
